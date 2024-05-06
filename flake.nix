6c3a26f0f5979b885d8c978cdb82ba9e71664d8f
Tomas Harkema <tomas@harkema.io>
2024-05-06 22:00:24 +0200

    fix package

--- flake.nix
+++ flake.nix
@@ -1,18 +1,8 @@
 {
   description = "Soothing pastel theme for Nix";
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-  };
-
-  outputs = {self, ...}: {
+  outputs = _: {
     homeManagerModules.catppuccin = import ./modules/home-manager;
     nixosModules.catppuccin = import ./modules/nixos;
-
-    overlays = {
-      pkgs = final: prev: {
-        catppuccin-gtk = self.inputs.nixpkgs.legacyPackages.${final.system}.catppuccin-gtk;
-      };
-    };
   };
 }
